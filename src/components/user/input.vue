6c957d941043ce4e72a5af5c4fb6e998169a11c9
liuqian0716 <1215381389@qq.com>
2017-06-13 00:13:47 +0800

    slot

--- src/components/user/input.vue
+++ src/components/user/input.vue
@@ -1,7 +1,8 @@
 <template>
 	<div>
-		<h1>刘茜</h1>
-    <input type="text" />
+		<div class="box">
+      <input type="text" /><img src="../../assets/img/search.png" alt="" />
+    </div>
 	</div>
 </template>
 
@@ -15,3 +16,9 @@ export default {
   }
 }
 </script> 
+<style>
+  *{margin:0;padding:0;}
+  .box{position:relative;width:200px;height:30px;margin:0 auto;}
+  .box input{width:200px;height:30px;}
+  .box img{position:absolute;top:10px;right:0;width:20px;height:20px;vertical-align: center;}
+</style>
