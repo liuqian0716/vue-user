d67fe55b39283928e91b63961aa3de98397c1f13
liuqian <1215381389@qq.com>
2017-07-31 17:10:07 +0800

    contiue

--- src/components/data/select.vue
+++ src/components/data/select.vue
@@ -3,12 +3,16 @@
     <select-input :is-show.sync="listShow" :title="title"></select-input>
     <list :data="listData" v-if="listShow" @changeTitle="titleHandle"></list>
     <h1>{{count}}</h1><button @click="mo">+</button>
+    <p>
+      <button @click="addAction">+</button>
+      <button @click="reduceAction">-</button>
+    </p>
   </div>
 </template>
 <script>
   import selectInput from './selectInput'
   import list from './list'
-  import { mapState, mapMutations, mapGetters } from 'vuex'
+  import { mapState, mapMutations, mapGetters, mapActions } from 'vuex'
   let listData = [
     {
       title: 'javascript'
@@ -40,6 +44,7 @@
     },
     methods: {
       ...mapMutations(['mo']),
+      ...mapActions(['addAction', 'reduceAction']),
       titleHandle (title) {
         console.log(title)
         this.title = title
@@ -51,17 +56,17 @@
       // }
     },
     computed: {
-      ...mapState(['count']),
-      ...mapGetters(['count'])
+      ...mapState(['count'])
+      // 第二种方法
+      // computed: mapState({
+      //   count: state => state.count
+      // })
+      // ...mapGetters(['count'])
       // 第二种写法
       // count () {
       //   return this.$store.getters.count
       // }
     }
-    // 第二种方法
-    // computed: mapState({
-    //   count: state => state.count
-    // })
   }
 </script>
 <style>
--- src/store/index.js
+++ src/store/index.js
@@ -16,12 +16,23 @@ let store = new Vuex.Store({
     sub (state, payload) {
       state.count -= payload.de
     },
-    mo (state) {
-      state.count += 100
+    mo (state, n) {
+      state.count += n
+    },
+    reduce (state) {
+      state.count --
     }
   },
   getters: {
     count: state => (state.count += 50)
+  },
+  actions: {
+    addAction (context) {
+      context.commit('mo', 10)
+    },
+    reduceAction ({commit}) {
+      commit('reduce')
+    }
   }
 })
 
