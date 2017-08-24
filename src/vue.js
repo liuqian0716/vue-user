7dffd813fd52bfa9fe1a8aff4e6b3a0961c19b49
liuqian <1215381389@qq.com>
2017-08-24 16:52:38 +0800

    0824

--- src/vue.js
+++ src/vue.js
@@ -4,11 +4,6 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-// axios.defaults.baseURL = 'https://api.example.com'
-// axios.defaults.headers.common['Authorization'] = 'xxxx'
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-// // axios.defaults.headers.common['Authorization'] = 'Bearer ' + window.localStorage.getItem('base64')
-// axios.defaults.timeout = 5000
 const instance = axios.create({
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
